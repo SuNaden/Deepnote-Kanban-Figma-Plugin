39e811a248865085d4ef2925c8558ba68af899a8
Filip Stollar <stollarf@gmail.com>
2020-03-28 20:54:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## Deepnote Kanban
+
+Deepnote tracks the status of designs by moving them into a page section in Figma. It's not fun dragging a page across 50 other pages and there are no shortcuts to help us with this afaik 😢
+
+This is a simple plugin to move the currently selected page to the right place (or to create a new page in playgrounds if you are cooking with something new).
+
+**Example**
+
+![](example.png)
+
+## How to run this
+
+1. You must have the Figma desktop app to run this
+2. Download this repo and unzip it
+3. Go to Plugins > Development > New Plugin
+4. Link an existing plugin by pointing Figma to the `manifest.json` in this folder
+5. Find it in Plugins > Development > Deepnote Kanban
---
+++ code.ts
@@ -0,0 +1,57 @@
+// This plugin will open a modal to prompt the user to enter a number, and
+// it will then create that many rectangles on the screen.
+
+// This file holds the main code for the plugins. It has access to the *document*.
+// You can access browser APIs in the <script> tag inside "ui.html" which has a
+// full browser enviroment (see documentation).
+
+// This shows the HTML page in "ui.html".
+figma.showUI(__html__);
+
+figma.ui.resize(280, 245);
+
+// Calls to "parent.postMessage" from within the HTML page will trigger this
+// callback. The callback will be passed the "pluginMessage" property of the
+// posted message.
+
+const getPageIndex = (pageName: string) => {
+  let implementPageIndex = 0;
+  for (let i = 0; i < figma.root.children.length; i++) {
+    if (figma.root.children[i].name.includes(pageName)) {
+      implementPageIndex = i;
+      break;
+    }
+  }
+
+  return implementPageIndex;
+};
+
+figma.ui.onmessage = msg => {
+  if (msg.type === "move-implement") {
+    const pageIndex = getPageIndex("--- To Implement");
+    const cur = figma.currentPage;
+    figma.root.insertChild(pageIndex + 1, cur);
+  }
+
+  if (msg.type === "move-review") {
+    const pageIndex = getPageIndex("--- To Review");
+    const cur = figma.currentPage;
+    figma.root.insertChild(pageIndex + 1, cur);
+  }
+
+  if (msg.type === "move-playgrounds") {
+    const pageIndex = getPageIndex("--- Playgrounds");
+    const cur = figma.currentPage;
+    figma.root.insertChild(pageIndex + 1, cur);
+  }
+
+  if (msg.type === "add-playgrounds") {
+    const pageIndex = getPageIndex("--- Playgrounds");
+    var newPage = figma.createPage();
+    figma.root.insertChild(pageIndex + 1, newPage);
+  }
+
+  // Make sure to close the plugin when you're done. Otherwise the plugin will
+  // keep running, which shows the cancel button at the bottom of the screen.
+  figma.closePlugin();
+};
